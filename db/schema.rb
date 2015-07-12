5facd14afdff7a6830f4f993ee5a359023e32c6d
Ruben Brands <rdbrands@gmail.com>
2015-07-12 20:45:46 +0200

    Added address to Restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150712183936) do
+ActiveRecord::Schema.define(version: 20150712184445) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.integer  "stars"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "address"
   end
 
 end
